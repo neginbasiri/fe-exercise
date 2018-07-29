e2c45682eb614a2d80f247533fcd04e9a7efa204
Negin Basiri <negin.basiri@empired.com>
2018-07-29 11:24:28 +1000

    no message

--- app/src/components/FilteredList/LinkButtons.js
+++ app/src/components/FilteredList/LinkButtons.js
@@ -1,5 +1,46 @@
 import React from 'react';
 
-const LinkButtons = links => <div className="link-buttons">{links.map(link => <a href="link">text</a>)}</div>;
+const findButtonText = link => {
+	let buttonText = '';
+	switch (link) {
+		case 'reddit_campaign':
+			buttonText = 'Reddit Campaign';
+			break;
+		case 'reddit_launch':
+			buttonText = 'Reddit Launch';
+			break;
+		case 'reddit_recovery':
+			buttonText = 'Reddit Recovery';
+			break;
+		case 'reddit_media':
+			buttonText = 'Reddit Media';
+			break;
+		case 'presskit':
+			buttonText = 'Press';
+			break;
+		case 'article_link':
+			buttonText = 'Article';
+			break;
+		case 'video_link':
+			buttonText = 'Watch Video';
+			break;
+		default:
+			buttonText = '';
+	}
+
+	return buttonText;
+};
+
+const LinkButtons = ({ links }) => {
+	return Object.keys(links).map((link, key) => {
+		if (link === 'mission_patch') return null;
+
+		return (
+			<a className="button" key={key} href={links[link]}>
+				{findButtonText(link)}
+			</a>
+		);
+	});
+};
 
 export default LinkButtons;
